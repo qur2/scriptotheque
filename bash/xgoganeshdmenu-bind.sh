52d41e8abab4b79314574d293b95b8b300a08430
Aurélien Scoubeau <aurelien.scoubeau@gmail.com>
2014-01-17 09:27:39 +0100

    xgoganeshdmenu correctly filter out duplicates.

--- bash/xgoganeshdmenu-bind.sh
+++ bash/xgoganeshdmenu-bind.sh
@@ -6,7 +6,7 @@ goganesh=$($gogan)
 wnames=$(exec "$xwindows")
 bins=$(dmenu_path)
 
-wname=$(printf "%s\n" "$goganesh" "$wnames" "$bins" | uniq | dmenu -nb '#151617' -nf '#d8d8d8' -sb '#d8d8d8' -sf '#151617') && \
+wname=$(printf "%s\n" "$goganesh" "$wnames" "$bins" | perl -ne '$H{$_}++ or print' | dmenu -nb '#151617' -nf '#d8d8d8' -sb '#d8d8d8' -sf '#151617') && \
 
 {
   wid=$(exec "$xwindows" "$wname")
